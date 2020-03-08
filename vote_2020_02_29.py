3edc22ccd74a5b88fe93e419d9f2457f673a3c35
Caleb James DeLisle <cjd@cjdns.fr>
2020-03-08 16:17:15 +0100

    Added a little script to display the sum of votes for each project

--- vote_2020_02_29.py
+++ vote_2020_02_29.py
@@ -248,3 +248,23 @@ votes.append({
 })
 
 print "RESULTS NOT YET FINAL - winners: %s" % getApprovedProjects(budget, costs, votes)
+
+projects = {}
+for x in votes[0]:
+    projects[x] = { 'short': 0., 'long': 0., 'scope': 0., 'risk': 0., 'hazard': 0. }
+for r in votes:
+    for x in r:
+        ## x = repo, price_display, etc
+        for y in projects[x]:
+          projects[x][y] += r[x][y]
+trans = {
+    'short':  "Short term impact         ",
+    'long':   "Long term impact          ",
+    'scope':  "Scope and use of resources",
+    'risk':   "Risk control              ",
+    'hazard': "Hazard control            "
+}
+for p in projects:
+    print p
+    for q in projects[p]:
+        print "  ", trans[q], projects[p][q]
